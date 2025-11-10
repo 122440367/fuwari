7676535adb8765e0b0659057b3a4482f951c0c61
122440367 <1224403167@qq.com>
2025-11-10 20:11:12 +0800

    submit

--- src/config.ts
+++ src/config.ts
@@ -40,11 +40,11 @@ export const siteConfig: SiteConfig = {
 	},
 	favicon: [
 		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
+		{
+		  src: 'https://472988.xyz/upload/QQ%E5%9B%BE%E7%89%8720250225193856.jpg',    // Path of the favicon, relative to the /public directory
+		//   theme: 'dark',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+		// //   sizes: '1080x1080',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		}
 	],
 };
 
---
+++ src/content/posts/为什么选择Fuwari.md
@@ -0,0 +1,27 @@
+---
+title: 为什么选择fuwari
+published: 2025-11-10
+description: '这是一个测试页面'
+image: 'https://blog.lecirtus.com/_astro/111.B35qqdyE_Z15ktKm.webp'
+tags: [markdown,博客]
+category: '文章'
+draft: false 
+lang: '' 
+---
+
+## 欢迎来到我的博客！
+
+本站注重分享知识，传播技巧，不定期更新，欢迎关注！
+
+## 为什么选择Fuwari
+
+其实原因很简单，因为本博客是一个静态网页，比较方便管理，也没有花里胡哨的功能，比较便于维护。此外，可以练习markdown格式的写法，故而新开这个博客
+
+
+
+<img src="https://img01.472988.xyz/20250404200158169.gif" style="zoom:25%;" />
+
+
+
+
+
